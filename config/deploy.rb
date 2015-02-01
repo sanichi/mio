ab7fb5b5dbd97329d3a924a1d0c8c9121f2cceb7
Mark Orr <mark.j.l.orr@googlemail.com>
2015-02-01 11:58:46 +0000

    Tweak

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/var/www/mio"
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :log_level, :debug
+set :log_level, :info
 
 # set :format, :pretty
 # set :pty, true
