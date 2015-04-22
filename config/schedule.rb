92dae9665fc9dc7715c86f0247358f04b3753c17
Mark Orr <mark.j.l.orr@googlemail.com>
2015-04-22 17:42:31 +0100

    Gzip the database dump files

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ set :output, "log/cron.log"
 set :job_template, nil
 
 every :day, at: "4:30 am" do
-  command "pg_dump sni_mio_app_production --clean --file ~/bak/db/mio-$(date +%Y-%m-%d).sql"
+  command "pg_dump sni_mio_app_production --clean | gzip > ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
 end
