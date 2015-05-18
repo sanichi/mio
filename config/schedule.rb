cc7256a3f8d0b3df255405f7d708ea11e4c1ea34
Mark Orr <mark.j.l.orr@googlemail.com>
2015-05-18 10:56:31 +0100

    Compress the backup databases

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ set :output, "#{path}/log/cron.log"
 set :job_template, nil
 
 every :day, at: "4:30 am" do
-  command "pg_dump sni_mio_app_production --clean --file ~/bak/db/mio-$(date +%Y-%m-%d).sql"
+  command "pg_dump sni_mio_app_production --clean --compress=1 --file ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
 end
