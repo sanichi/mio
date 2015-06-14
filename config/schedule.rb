de5dc074c37399fcbaa807207bca263e653adf89
Mark Orr <mark.j.l.orr@googlemail.com>
2015-06-14 16:46:45 +0100

    Correction

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,5 +6,5 @@ every :day, at: "4:30 am" do
 end
 
 every :day, at: "5:00 am" do
-  command "cd #{path}; tar -zcvf ~/bak/images/mio-$(date +%Y-%m-%d).tar.gz `find public/system/pictures -print | grep '/original/'`
+  command "cd #{path}; tar -zcvf ~/bak/images/mio-$(date +%Y-%m-%d).tar.gz `find public/system/pictures -print | grep '/original/'`"
 end
