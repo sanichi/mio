ba5cfefbf7130ecc4f9547e11e17b1e8a2d80ae2
Mark Orr <mark.j.l.orr@googlemail.com>
2015-06-27 18:04:42 +0100

    Tweal to DB backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :output, "#{path}/log/cron.log"
 set :job_template, nil
 
 every :day, at: "4:30 am" do
-  command "pg_dump sni_mio_app_production --clean --compress=1 --file ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
+  command "pg_dump sni_mio_app_production --no-owner --no-privileges --compress=1 --clean --file ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
 end
 
 every :day, at: "5:00 am" do
