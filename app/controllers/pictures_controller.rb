3b73400400e964ab2da301295793ec31f8bf5c8d
Mark Orr <mark.j.l.orr@googlemail.com>
2015-08-07 16:38:24 +0100

    Tweak

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,7 +9,7 @@ class PicturesController < ApplicationController
 
   def new
     person_id = params[:person_id].to_i
-    person_id = nil unless Person.find_by(id: person_id)
+    person_id = nil unless person_id > 0 && Person.find_by(id: person_id)
     @picture = Picture.new(person_id: person_id)
   end
 
