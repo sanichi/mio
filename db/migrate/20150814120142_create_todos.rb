f44e76ef350b9da4b36c20c6ca8ce5326a88d6e9
Mark Orr <mark.j.l.orr@googlemail.com>
2015-09-22 20:26:04 +0100

    Tweak

--- db/migrate/20150814120142_create_todos.rb
+++ db/migrate/20150814120142_create_todos.rb
@@ -3,7 +3,7 @@ class CreateTodos < ActiveRecord::Migration
     create_table :todos do |t|
       t.string   :description, limit: Todo::MAX_DESC
       t.boolean  :done, default: false
-      t.integer  :priority, limit: 1, default: 0
+      t.integer  :priority, limit: 1, default: Todo::PRIORITIES.first
 
       t.timestamps null: false
     end
