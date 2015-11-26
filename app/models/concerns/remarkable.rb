ccd6a024f46bd96ef45e05831dedc15e64c87f54
Mark Orr <mark.j.l.orr@googlemail.com>
2015-11-26 19:53:48 +0000

    Add syntax highlighting for the blog

--- app/models/concerns/remarkable.rb
+++ app/models/concerns/remarkable.rb
@@ -2,7 +2,16 @@ module Remarkable
   def to_html(text, filter_html: false)
     return "" unless text.present?
     renderer = Redcarpet::Render::HTML.new(filter_html: filter_html)
-    markdown = Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true, strikethrough: true, underline: true, tables: true)
+    options =
+    {
+      autolink: true,
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      tables: true,
+      underline: true,
+    }
+    markdown = Redcarpet::Markdown.new(renderer, options)
     markdown.render(text).html_safe
   end
 end
