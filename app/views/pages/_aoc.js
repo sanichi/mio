b33d1a2d273e28fe39f5be2478bab47e3a553d06
Mark Orr <mark.j.l.orr@googlemail.com>
2016-01-04 21:32:04 +0000

    Add year to AOC form

--- app/views/pages/_aoc.js
+++ app/views/pages/_aoc.js
@@ -5,21 +5,29 @@ $(function() {
   elm_app = Elm.worker(Elm.Main, { problem: [2015, 1, ""] });
   elm_app.ports.answer.subscribe(display_aoc_answer);
 
-  // Send a problem to the Elm app every time the menus update.
+  // See if we can send a problem to the Elm app every time the menus update.
+  $('#year').change(function() {
+    solve_aoc_problem($(this).val(), $('#day').val());
+  });
   $('#day').change(function() {
-    var day = parseInt($(this).val(), 10);
-    if (!isNaN(day)) solve_aoc_problem(2015, day);
+    solve_aoc_problem($('#year').val(), $(this).val());
   });
 });
 
-// Get the input for this problem, then send it, along with year and day, to the Elm app.
-function solve_aoc_problem(year, day) {
-  var file = '/aoc/' + year + '/' + day + '.txt';
-  $.ajax({url: file}).done(function(text) {
-    $('#input').val(text);
-    $('#loading').show();
-    elm_app.ports.problem.send([year, day, text]);
-  });
+// Check both a year and a day are selected and if so, get the input
+// for the problem and send it, along with year and day, to the Elm app.
+function solve_aoc_problem(y, d) {
+  var year = parseInt(y, 10);
+  var day  = parseInt(d, 10);
+  if (!isNaN(day) && !isNaN(year))
+  {
+    var file = '/aoc/' + year + '/' + day + '.txt';
+    $.ajax({url: file}).done(function(text) {
+      $('#input').val(text);
+      $('#loading').show();
+      elm_app.ports.problem.send([year, day, text]);
+    });
+  }
 }
 
 // When the Elm app sends back a solution, display it.
