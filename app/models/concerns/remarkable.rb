dada8935d7adaa0234889524dfd6709c62a57c22
Mark Orr <mark.j.l.orr@googlemail.com>
2016-02-13 13:00:25 +0000

    Customised redcarpet

--- app/models/concerns/remarkable.rb
+++ app/models/concerns/remarkable.rb
@@ -1,7 +1,27 @@
 module Remarkable
+  class CustomRenderer < Redcarpet::Render::HTML
+    LINK_WITH_TARGET = /\A(.+)\|(\w+)\z/
+
+    def link(link, title, alt_text)
+      if link.match LINK_WITH_TARGET
+        '<a href="%s" target="%s">%s</a>' % [$1, $2, alt_text]
+      else
+        '<a href="%s">%s</a>' % [link, alt_text]
+      end
+    end
+
+    def autolink(link, link_type)
+      if link.match LINK_WITH_TARGET
+        '<a href="%s" target="%s">%s</a>' % [$1, $2, $1]
+      else
+        '<a href="%s">%s</a>' % [link, link]
+      end
+    end
+  end
+
   def to_html(text, filter_html: false)
     return "" unless text.present?
-    renderer = Redcarpet::Render::HTML.new(filter_html: filter_html)
+    renderer = CustomRenderer.new(filter_html: filter_html)
     options =
     {
       autolink: true,
