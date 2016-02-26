317040bb822ca16583c1f0da20639e037d387ff4
Mark Orr <mark.j.l.orr@googlemail.com>
2016-02-26 15:45:08 +0000

    Correction and simplification

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -7,13 +7,13 @@ class ParkingsController < ApplicationController
 
   def new
     @parking = Parking.new
+    bays_for_buttons
     if (vid = params[:vehicle].to_i) > 0
       @parking.vehicle_id = vid
     end
     if (bid = params[:bay].to_i) > 0
       @parking.bay_id = bid
     end
-    @bay = Bay.all.each_with_object({}) { |b, h| h[b.number] = b }
   end
 
   def create
@@ -21,6 +21,7 @@ class ParkingsController < ApplicationController
     if @parking.save
       redirect_to parkings_path
     else
+      bays_for_buttons
       render action: "new"
     end
   end
@@ -34,5 +35,9 @@ class ParkingsController < ApplicationController
 
   def strong_params
     params.require(:parking).permit(:vehicle_id, :bay_id)
+  end
+
+  def bays_for_buttons
+    @bay = Bay.all.each_with_object({}) { |b, h| h[b.number] = b }
   end
 end
