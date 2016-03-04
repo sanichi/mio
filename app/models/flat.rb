caed821ef999652acdaab9005ca7457e7df0c7fb
Mark Orr <mark.j.l.orr@googlemail.com>
2016-03-04 08:28:11 +0000

    Correction

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -7,7 +7,7 @@ class Flat < ActiveRecord::Base
   BUILDINGS = (5..21).to_a
   NUMBERS = (1..9).to_a
   CATEGORIES = %w/A A1 A2 A3 A4 B B1 B2 BM B-upper C C1 CM E E1 P1 P3/
-  NAMES = %w/Albany Clofars Comet Concord Dart Eagle Fortune Hopewell Leith Penthouse Raith Rohilla Ronan Salamander Sirius Unicorn/
+  NAMES = %w/Albany Clofars Comet Concord Dart Eagle Fortune Hopewill Leith Penthouse Raith Rohilla Ronan Salamander Sirius Unicorn/
 
   belongs_to :owner, class_name: "Resident", foreign_key: "owner_id"
   belongs_to :tenant, class_name: "Resident", foreign_key: "tenant_id"
