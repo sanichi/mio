1f33eaea92da1640fb2c7fb47adced99eb5212c2
Mark Orr <mark.j.l.orr@googlemail.com>
2016-03-04 16:33:07 +0000

    Correction

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -2,8 +2,8 @@ class Resident < ActiveRecord::Base
   include Pageable
 
   has_many :vehicles, dependent: :nullify
-  has_many :ownerships, class_name: "Flat", foreign_key: "owner_id"
-  has_many :tenancies, class_name: "Flat", foreign_key: "tenant_id"
+  has_many :ownerships, class_name: "Flat", foreign_key: "owner_id", dependent: :nullify
+  has_many :tenancies, class_name: "Flat", foreign_key: "tenant_id", dependent: :nullify
 
   before_validation :canonicalize
 
