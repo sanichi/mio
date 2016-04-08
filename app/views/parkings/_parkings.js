d6df890b113d492f80605264e107e71dc7036a32
Mark Orr <mark.j.l.orr@googlemail.com>
2016-04-08 16:23:38 +0100

    Auto-complete on parking search as well

--- app/views/parkings/_parkings.js
+++ app/views/parkings/_parkings.js
@@ -1,15 +1,31 @@
 $(function() {
+  // Reset button.
   $('#reset').click(function(e) {
     e.preventDefault();
+    $('#term').val('');
     $('#vehicle').val('');
     $('#bay').val('');
     $(this).parents('form').submit();
   });
+  // Click on bays.
   $('g.bay').click(function(e) {
     $('#bay').val($(this).data('bay')).parents('form').submit();
   });
+  // Click on roads.
   $('g.road').click(function(e) {
     $('#bay').val('0').parents('form').submit();
   });
+  // Auto-completer for vehicles.
+  $('input[name="term"]').autocomplete({
+    source: '/vehicles/match.json',
+    minLength: 2,
+    select: function(event, ui) {
+      $('#vehicle').val(ui.item ? ui.item.id : '');
+      setTimeout(function() {
+        $('input[name="term"]').val('');
+      }, 200);
+    }
+  });
+  // Intelligently guess what "New Parking" should do.
   $('a[href^="/parkings/new"]').attr('href','<%= guess_next_parking(@parkings.matches) %>');
 });
