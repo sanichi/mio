ab16b42905c6829fd92eff4570ae60fa8b8ddb31
Mark Orr <mark.j.l.orr@googlemail.com>
2016-06-14 07:21:37 +0100

    Let cookies survive longer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,4 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store :cookie_store, key: '_sni_mio_app_session'
+Rails.application.config.session_store :cookie_store,
+  key: "_sni_mio_app_session",
+  expire_after: 2.weeks
