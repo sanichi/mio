07bf88954e757adbba87ceb6055f1377702c8bdf
Mark Orr <mark.j.l.orr@googlemail.com>
2017-02-15 15:00:05 +0000

    Don’t backup quite so much

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 set :output, "#{path}/log/cron.log"
 set :job_template, nil
 
-every :day, at: "4:30 am" do
+every :sunday, at: "4:30 am" do
   command "pg_dump sni_mio_app_production --no-owner --no-privileges --compress=1 --clean --file ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
 end
 
-every :day, at: "5:00 am" do
+every :sunday, at: "5:00 am" do
   command "cd #{path}; tar -zcvf ~/bak/images/mio-$(date +%Y-%m-%d).tar.gz `find public/system/pictures -print | grep '/original/'`"
 end
