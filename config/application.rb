57f24689442fd05bf9952f0a6a0b0642733dd521
Mark Orr <mark.j.l.orr@googlemail.com>
2017-05-12 21:49:35 +0100

    Minor updates to conform with 5.1

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,12 @@ Bundler.require(*Rails.groups)
 
 module SniMioApp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    # This causes problems for me - see also initializers/new_framework_defaults.rb (TODO).
+    # config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Include /lib in the autoload path.
-    # config.autoload_paths << "#{Rails.root}/lib"
   end
 end
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -5,3 +5,10 @@
 
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = false
+
+# See also the Rails config.load_defaults method (since 5.1). I does thigs like this:
+# Rails.application.config.active_record.belongs_to_required_by_default = true
+# Rails.application.config.action_controller.per_form_csrf_tokens = true
+# Rails.application.config.action_controller.forgery_protection_origin_check = true
+# Rails.application.config.assets.unknown_asset_fallback = false
+# But it causes problems for me so is currently commented out in application.rb.
