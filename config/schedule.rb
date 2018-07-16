6613a2f0f58cb317570b20466b13e35bff8e4896
Mark Orr <mark.j.l.orr@googlemail.com>
2018-07-16 17:45:08 +0100

    No need to backup images directory

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,3 @@ set :job_template, nil
 every :sunday, at: "4:30 am" do
   command "pg_dump sni_mio_app_production --no-owner --no-privileges --compress=1 --clean --file ~/bak/db/mio-$(date +%Y-%m-%d).sql.gz"
 end
-
-every :sunday, at: "5:00 am" do
-  command "cd #{path}; tar -zcvf ~/bak/images/mio-$(date +%Y-%m-%d).tar.gz `find public/system/pictures -print | grep '/original/'`"
-end
