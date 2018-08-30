5bdd915caf12f2e1dd22468724dbc71259e21b8d
Mark Orr <mark.j.l.orr@googlemail.com>
2018-08-30 20:30:25 +0100

    Got rid of elm retina

--- app/views/pages/_retina.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-  var node = $('#elm-app').get(0);
-  var app = Elm.Main.embed(node);
-});
--- app/views/pages/retina/Messages.elm
+++
@@ -1,6 +0,0 @@
-module Messages exposing (..)
-
-
-type Msg
-    = NoOp
-    | NewValues (List Float)
