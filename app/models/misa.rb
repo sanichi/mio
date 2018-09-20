84a9df25456a7b0534d60a0af619bc126c969c11
Mark Orr <mark.j.l.orr@googlemail.com>
2018-09-20 11:14:42 +0100

    New misa category

--- app/models/misa.rb
+++ app/models/misa.rb
@@ -3,7 +3,7 @@ class Misa < ApplicationRecord
   include Pageable
   include Remarkable
 
-  CATEGORIES = %w/none beginners counters dajare difference dogen howto mistakes native/
+  CATEGORIES = %w/none beginners counters dajare difference dogen howto mistakes native shadowing/
   MAX_CATEGORY = 10
   MAX_MINUTES = 6
   MAX_SHORT = 15
