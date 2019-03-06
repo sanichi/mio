c13d3a16e403fffa9c967cd9f9551e6e62a4f174
Mark Orr <mark.j.l.orr@googlemail.com>
2019-03-06 10:11:40 +0000

    General gem update

--- spec/factories/misas.rb
+++ spec/factories/misas.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :misa do
     category  { Misa::CATEGORIES.sample }
     japanese  { [true, false].sample }
-    long      { [Faker::Bitcoin.address.truncate(11, omission: ""), nil].sample }
+    long      { [Faker::Blockchain::Bitcoin.address.truncate(11, omission: ""), nil].sample }
     minutes   { "#{rand(1000)}:#{sprintf('%02d', rand(60))}" }
     note      { Faker::Lorem.paragraph(3) }
     published { Faker::Date.between(5.years.ago, Date.today) }
-    short     { Faker::Bitcoin.address.truncate(11, omission: "") }
+    short     { Faker::Blockchain::Bitcoin.address.truncate(11, omission: "") }
     title     { Faker::Lorem.paragraph.truncate(Note::MAX_TITLE) }
   end
 end
