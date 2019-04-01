1e485a59d28c7301f59591f204613e261ea3d817
Mark Orr <mark.j.l.orr@googlemail.com>
2019-04-01 08:11:18 +0100

    Another way to show the correct answer

--- app/views/questions/_answers.js
+++ app/views/questions/_answers.js
@@ -5,4 +5,7 @@ $(function() {
   $('#question').on('mouseout', 'li.correct-answer', function() {
     $(this).removeClass("text-light bg-dark");
   });
+  $('#question').on('click', '#solution', function() {
+    $(this).text($(this).data('solution'));
+  });
 });
