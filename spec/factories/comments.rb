8eeb8c3bcfce9b52a4ebe9d0bdc80f7fe7aa0f31
Mark Orr <mark.j.l.orr@googlemail.com>
2019-12-18 15:09:17 +0000

    No use for comments anymore

--- spec/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    source { Faker::Name.name.truncate(Comment::MAX_SOURCE) }
-    date   { Faker::Date.backward(days: 365) }
-    text   { Faker::Lorem.paragraphs(number: 2) }
-  end
-end
