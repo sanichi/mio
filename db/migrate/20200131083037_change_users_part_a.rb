665787d6bbafc4ca1cdb166f31f88ebdde1d5440
Mark Orr <mark.j.l.orr@googlemail.com>
2020-01-31 10:53:07 +0000

    Fix migration

--- db/migrate/20200131083037_change_users_part_a.rb
+++ db/migrate/20200131083037_change_users_part_a.rb
@@ -4,12 +4,7 @@ class ChangeUsersPartA < ActiveRecord::Migration[6.0]
     add_column :users, :last_name, :string, limit: User::MAX_LN
 
     User.all.each do |u|
-      p = u.person
-      if p
-        u.update_columns(first_name: p.first_names, last_name: p.last_name)
-      else
-        u.update_columns(first_name: "Unknown", last_name: "Unknown")
-      end
+      u.update_columns(first_name: "Unknown", last_name: "Unknown")
     end
   end
 end
