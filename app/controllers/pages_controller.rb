65981804071305e78e14f28e44937196f1294908
Mark Orr <mark.j.l.orr@googlemail.com>
2020-04-20 13:51:58 +0100

    Remove old code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,6 @@ class PagesController < ApplicationController
     @flats = Flat.where.not(bay: nil).order(:bay).all
   end
 
-  def check
-    respond_to do |format|
-      format.json { render json: ::Checker.check }
-    end
-  end
-
   def pam
     @answer = Flat.pam(params[:q])
   end
