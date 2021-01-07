6604d6f5331f0630bab207899fbc8f53be62504d
Mark Orr <mark.j.l.orr@googlemail.com>
2021-01-07 19:32:43 +0000

    Got rid of active_storage for people pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,33 +2,21 @@ class Picture < ApplicationRecord
   include Constrainable
   include Remarkable
 
-  MAX_SIZE = (Rails.env.test?? 1 : 50).megabytes
   MAX_TITLE = 50
   SIZE = { tn: 100, xs: 300, sm: 600, md: 900, lg: 1200 }
-  STYLE = SIZE.each_with_object({}) do |(nm,sz),v|
-    if nm == :tn
-      v[nm] = { thumbnail: "#{sz}x#{sz}^", gravity: "center", extent: "#{sz}x#{sz}" }
-    else
-      v[nm] = { resize: "#{sz}x#{sz}>" }
-    end
-  end
-  TYPES = "jpe?g|gif|png"
 
   has_and_belongs_to_many :people
-
-  has_one_attached :image, dependent: :purge
 
   before_validation :normalize_attributes
 
   validates :realm, numericality: { integer_only: true, greater_than_or_equal_to: Person::MIN_REALM, less_than_or_equal_to: Person::MAX_REALM }
-  validate :check_image_attachment
   validate :check_people_realm
 
   default_scope { order(id: :desc) }
 
   def self.search(params)
     sql = nil
-    matches = joins(:people).includes(:image_attachment).includes(:image_blob)
+    matches = joins(:people)
     matches = matches.where(sql) if sql = cross_constraint(params[:name], %w(last_name first_names known_as married_name), table: :people)
     matches = matches.where(sql) if sql = cross_constraint(params[:description], %w{description})
     matches = matches.where(realm: params[:realm].to_i)
@@ -48,18 +36,12 @@ class Picture < ApplicationRecord
     self.people = new_ids.map{ |id| Person.find_by(id: id) }.compact
   end
 
-  # Not used anymore because the indirection makes it slow.
-  def thumbnail_path
-    Rails.application.routes.url_helpers.rails_representation_url(image.variant(STYLE[:tn]), only_path: true)
-  end
-
-  # This is faster. Based on: https://stackoverflow.com/questions/50340043/get-path-to-activestorage-file-on-disk.
-  # However, it doesn't create the variant file - so it's necessary to use a more standard path to do the creation.
-  # That's why the thumbnail variant is now included in show.html.
-  def direct_thumbnail_path
-    root = Rails.root + "public"
-    path = Pathname.new(ActiveStorage::Blob.service.send(:path_for, image.variant(STYLE[:tn]).key))
-    "/#{path.relative_path_from(root)}"
+  def path(thumbnail: true)
+    fend = "jpg"
+    fend = "png" if [360,359,358,353,352,62,61,60,9].include?(id)
+    tumb =
+    tumb = "t" if thumbnail
+    "/people/#{id}#{tumb}.#{fend}"
   end
 
   def self.hidden_class(nm)
@@ -100,16 +82,6 @@ class Picture < ApplicationRecord
       I18n.t("picture.picture")
     else
       people.sort{ |a, b| a.known_as <=> b.known_as }.map{ |p| p.name(full: false) }.join(", ").truncate(MAX_TITLE);
-    end
-  end
-
-  def check_image_attachment
-    if image.attached?
-      errors.add(:image, "invalid content type (#{image.content_type})") unless image.content_type =~ /\Aimage\/(#{TYPES})\z/
-      errors.add(:image, "invalid filename (#{image.filename})")         unless image.filename.to_s =~ /\.(#{TYPES})\z/i
-      errors.add(:image, "too large an image size (#{image.byte_size})") unless image.byte_size < MAX_SIZE
-    else
-      errors.add(:image, "no image attached")
     end
   end
 
--- spec/features/picture_spec.rb
+++ spec/features/picture_spec.rb
@@ -6,9 +6,6 @@ describe Picture do
   let!(:person2) { create(:person, realm: data.realm) }
   let!(:person3) { create(:person, realm: data.realm) }
 
-  let(:good_file)  { "malcolm.jpg" }
-  let(:good_file2) { "malcolm.png" }
-
   before(:each) do
     login
     click_link t("person.people")
@@ -26,7 +23,6 @@ describe Picture do
         click_link t("picture.new")
         fill_in t("description"), with: data.description
         check t("picture.portrait") if data.portrait
-        attach_file t("picture.file"), test_file_path(good_file)
         select person1.name(reversed: true, with_years: true), from: t("picture.person", number: 1)
         select person2.name(reversed: true, with_years: true), from: t("picture.person", number: 2)
         select t("person.realms")[data.realm], from: t("person.realm")
@@ -45,16 +41,12 @@ describe Picture do
         expect(p.people).to be_include(person1)
         expect(p.people).to be_include(person2)
         expect(p.title).to eq title
-        expect(p.image.filename).to eq good_file
-        expect(p.image.content_type).to eq "image/jpeg"
-        expect(p.image.byte_size).to eq 13738
       end
 
       it "without people" do
         click_link t("picture.new")
         fill_in t("description"), with: data.description
         check t("picture.portrait") if data.portrait
-        attach_file t("picture.file"), test_file_path(good_file)
         select t("person.realms")[data.realm], from: t("person.realm")
         click_button t("save")
 
@@ -69,36 +61,13 @@ describe Picture do
         expect(p.portrait).to eq data.portrait
         expect(p.people.size).to eq 0
         expect(p.title).to eq title
-        expect(p.image.filename).to eq good_file
-        expect(p.image.content_type).to eq "image/jpeg"
-        expect(p.image.byte_size).to eq 13738
-      end
-    end
-
-    context "failure" do
-      let(:bad_file) { "malcolm.txt" }
-
-      it "bad name" do
-        click_link t("picture.new")
-        fill_in t("description"), with: data.description
-        check t("picture.portrait") if data.portrait
-        attach_file t("picture.file"), test_file_path(bad_file)
-        select person1.name(reversed: true, with_years: true), from: t("picture.person", number: 1)
-        select t("person.realms")[data.realm], from: t("person.realm")
-        click_button t("save")
-
-        expect(page).to have_title t("picture.new")
-        expect(Picture.count).to eq 0
-        expect(PersonPicture.count).to eq 0
-        expect_error(page, "invalid filename")
       end
     end
   end
 
-  context "edit", type: :active_storage do
+  context "edit" do
     let!(:picture) do
       p = Picture.new(people: [person1, person2], description: data.description, realm: data.realm)
-      p.image.attach(io: File.open(test_file_path(good_file)), filename: good_file)
       p.save
       p
     end
@@ -141,31 +110,11 @@ describe Picture do
       picture.reload
       expect(picture.description).to be_nil
     end
-
-    it "image" do
-      expect(picture.image.filename).to eq good_file
-      expect(picture.image.content_type).to eq "image/jpeg"
-      expect(picture.image.byte_size).to eq 13738
-
-      expect(page).to have_title t("picture.edit")
-      attach_file t("picture.file"), test_file_path(good_file2)
-      click_button t("save")
-
-      expect(page).to_not have_title t("picture.edit")
-      expect(Picture.count).to eq 1
-      expect(PersonPicture.count).to eq 2
-
-      picture.reload
-      expect(picture.image.filename).to eq good_file2
-      expect(picture.image.content_type).to eq "image/png"
-      expect(picture.image.byte_size).to eq 8285
-    end
   end
 
   context "delete" do
     let!(:picture) do
       p = Picture.new(people: [person1, person2, person3], description: data.description, realm: data.realm)
-      p.image.attach(io: File.open(test_file_path(good_file)), filename: good_file)
       p.save
       p
     end
