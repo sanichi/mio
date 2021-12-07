f0d3f0db51442cf0d329fdd044afdbd919d62197
Mark Orr <mark.j.l.orr@googlemail.com>
2021-12-07 17:26:36 +0000

    Order grammars better for prev and next

--- app/controllers/grammars_controller.rb
+++ app/controllers/grammars_controller.rb
@@ -31,8 +31,10 @@ class GrammarsController < ApplicationController
   def show
     if @grammar
       @examples = @grammar.update_examples.reverse
-      @next = Grammar.find_by(id: @grammar.id + 1)
-      @prev = Grammar.find_by(id: @grammar.id - 1)
+      ids = Grammar.order(:ref).pluck(:id)
+      ind = ids.index(@grammar.id)
+      @next = Grammar.find_by(id: ids[ind + 1]) || Grammar.find_by(id: ids[0])
+      @prev = Grammar.find_by(id: ids[ind - 1]) || Grammar.find_by(id: ids[-1])
     end
   end
 
