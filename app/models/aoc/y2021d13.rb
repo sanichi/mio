b9867710594dd0ec0f05514d5a9a2884d0fc6228
Mark Orr <mark.j.l.orr@googlemail.com>
2021-12-13 16:44:03 +0000

    Day 13

--- app/models/aoc/y2021d13.rb
+++ app/models/aoc/y2021d13.rb
@@ -1,6 +1,79 @@
 class Aoc::Y2021d13 < Aoc
   def answer(part)
-    "not done yet"
+    paper = Paper.new(input)
+    if part == 1
+      paper.fold_first
+    else
+      paper.fold_all
+    end
+  end
+
+  class Paper
+    attr_reader :dots, :instructions, :width, :height
+
+    def initialize(string)
+      @dots = {}
+      @instructions = []
+      @width = nil
+      @height = nil
+      string.each_line(chomp: true) do |line|
+        if line.match(/\A(\d+),(\d+)\z/)
+          x = $1.to_i
+          y = $2.to_i
+          @dots[[x,y]] = true
+        elsif line.match(/\Afold along (x|y)=(\d+)/)
+          s = $2.to_i
+          if $1 == "x"
+            @instructions.push s
+            @width = 2 * s + 1 unless @width
+          else
+            @instructions.push -s
+            @height = 2 * s + 1 unless @height
+          end
+        else
+          raise "invalid line" unless line == ""
+        end
+      end
+    end
+
+    def fold_first
+      fold(instructions.first)
+      dots.values.select{|v| v}.size
+    end
+
+    def fold_all
+      instructions.each{|i| fold(i)}
+      (0..height-1).each do |y|
+        Rails.logger.info (0..width-1).map{|x| dots[[x,y]] ? "#" : '.'}.join
+      end
+      "EPZGKCHU" # from visual inspection of log output
+    end
+
+    private
+
+    def fold(i)
+      i > 0 ? fold_left(i) : fold_up(i.abs)
+    end
+
+    def fold_left(x)
+      @width = x
+      dots.keys.each do |i,j|
+        if i > width
+          dots[[2 * width - i,j]] = true
+          dots.delete [i,j]
+        end
+      end
+    end
+
+    def fold_up(y)
+      @height = y
+      dots.keys.each do |i,j|
+        if j > height
+          dots[[i, 2 * height - j]] = true
+          dots.delete [i,j]
+        end
+      end
+    end
   end
 
   EXAMPLE = <<~EOE
