daf0a7c4a5af3df73f131b2dc7456e6acfad3307
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-23 09:20:18 +0000

    CSP downgrade for Kanji kvg_xml

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     policy.img_src         :self, :data
     policy.media_src       :self, "https://files.wanikani.com/"
     policy.script_src      :self
-    policy.style_src       :self
+    policy.style_src       :self, :unsafe_inline # redcarpet, kanji kvg_xml
     policy.base_uri        :self
     policy.frame_ancestors :none
     policy.form_action     :self
