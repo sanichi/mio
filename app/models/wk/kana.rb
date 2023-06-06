7e30e08673af47415ba1bba583f80930d5820021
Mark Orr <mark.j.l.orr@googlemail.com>
2023-06-06 08:35:04 +0100

    Refactored common accent code

--- app/models/wk/kana.rb
+++ app/models/wk/kana.rb
@@ -1,5 +1,6 @@
 module Wk
   class Kana < ApplicationRecord
+    include Accentable
     include Constrainable
     include Pageable
     include Remarkable
@@ -76,43 +77,6 @@ module Wk
 
     def parts_of_speech
       parts.split(",").map{ |p| I18n.t("wk.parts.#{p}").downcase }.join(", ")
-    end
-
-    def accent_display
-      case accent_position
-      when nil                  then "?"
-      when Wk::Reading::UNKNOWN then "?"
-      else accent_position.to_s
-      end
-    end
-
-    def pattern_colour
-      if accent_pattern.present?
-        %w/secondary success warning info/[accent_pattern] || "danger"
-      elsif accent_position == Wk::Reading::UNKNOWN
-        "secondary"
-      else
-        "outline-secondary"
-      end
-    end
-
-    # to be able to remove old color classes in wk/kanas/quick_accent_update.js.erb
-    def pattern_colours
-      %w/secondary success warning info danger outline-secondary/
-    end
-
-    # this is what gets run when a quick_accent_update ajax request is procesed
-    # note that validation (see set_accent_pattern below) kicks in because of the save
-    def update_accent(new_accent)
-      if new_accent == "?"
-        self.accent_position = nil
-      elsif new_accent == "-"
-        self.accent_position = UNKNOWN
-      else
-        i = new_accent.to_i
-        self.accent_position = i if i >= 0
-      end
-      save
     end
 
     def self.update(days=nil)
@@ -247,44 +211,6 @@ module Wk
     end
 
     private
-
-    def set_accent_pattern
-      if changes.has_key?("characters") || changes.has_key?("accent_position")
-        if accent_position.is_a?(Integer)
-          morae = count_morae
-          self.accent_position = morae if accent_position > morae
-          self.accent_position = UNKNOWN if accent_position < Wk::Reading::MIN_ACCENT_POSITION
-          self.accent_pattern =
-            case accent_position
-            when Wk::Reading::UNKNOWN
-              nil
-            when 0
-              Wk::Reading::HEIBAN
-            when 1
-              Wk::Reading::ATAMADAKA
-            when morae
-              Wk::Reading::ODAKA
-            else
-              Wk::Reading::NAKADAKA
-            end
-        else
-          self.accent_pattern = nil
-        end
-      end
-    end
-
-    def count_morae
-      # get a throwaway copy of the reading, handling the nil case
-      string = characters.to_s.dup
-      # make sure to get only the first reading if there's more than one
-      string.sub!(/,.*/, "")
-      # count 1 for all hiragana, katakana and the katakana elongation symbol
-      full = string.each_char.map{ |c| c =~ /\A[ぁ-んァ-ンー]\z/ ? 1 : 0 }.sum
-      # count 1 for all small hiragana and katakana (but not the っ or ッ)
-      tiny = string.each_char.map{ |c| c =~ /\A[ぁァぃィぅゥぇェぉォゃャゅュょョ]\z/ ? 1 : 0 }.sum
-      # the number of morae is the difference between these two
-      full - tiny
-    end
 
     def clean_up
       self.notes = nil unless notes.present?
--- app/models/wk/reading.rb
+++ app/models/wk/reading.rb
@@ -1,21 +1,7 @@
 module Wk
   class Reading < ActiveRecord::Base
+    include Accentable
     include Wanikani
-
-    # pitch accent patterns
-    HEIBAN = 0
-    ATAMADAKA = 1
-    NAKADAKA = 2
-    ODAKA = 3
-
-    # special accent_position which means we've looked but can't find out what it should be
-    # when accent_position is nil, it means we haven't even tried to find out yet
-    UNKNOWN = -1
-
-    MAX_CHARACTERS = 24
-    MAX_ACCENT_PATTERN = ODAKA
-    MIN_ACCENT_PATTERN = HEIBAN
-    MIN_ACCENT_POSITION = UNKNOWN
 
     before_validation :set_accent_pattern
 
@@ -27,43 +13,6 @@ module Wk
     has_many :audios, as: :audible, dependent: :destroy
 
     default_scope { order(primary: :desc) }
-
-    def accent_display
-      case accent_position
-      when nil     then "?"
-      when UNKNOWN then "?"
-      else accent_position.to_s
-      end
-    end
-
-    def pattern_colour
-      if accent_pattern.present?
-        %w/secondary success warning info/[accent_pattern] || "danger"
-      elsif accent_position == UNKNOWN
-        "secondary"
-      else
-        "outline-secondary"
-      end
-    end
-
-    # to be able to remove old color classes in wk/readings/quick_accent_update.js.erb
-    def pattern_colours
-      %w/secondary success warning info danger outline-secondary/
-    end
-
-    # this is what gets run when a quick_accent_update ajax request is procesed
-    # note that validation (see set_accent_pattern below) kicks in because of the save
-    def update_accent(new_accent)
-      if new_accent == "?"
-        self.accent_position = nil
-      elsif new_accent == "-"
-        self.accent_position = UNKNOWN
-      else
-        i = new_accent.to_i
-        self.accent_position = i if i >= 0
-      end
-      save
-    end
 
     def self.update(days=nil)
       stats = Hash.new(0)
@@ -183,46 +132,6 @@ module Wk
       stats.sort_by { |k, v| v }.reverse.each do |k, v|
         puts "  %s%s %s %s" % [k, "." * (25 - k.length), v, extra[k]]
       end
-    end
-
-    private
-
-    def set_accent_pattern
-      if changes.has_key?("characters") || changes.has_key?("accent_position")
-        if accent_position.is_a?(Integer)
-          morae = count_morae
-          self.accent_position = morae if accent_position > morae
-          self.accent_position = UNKNOWN if accent_position < MIN_ACCENT_POSITION
-          self.accent_pattern =
-            case accent_position
-            when UNKNOWN
-              nil
-            when 0
-              HEIBAN
-            when 1
-              ATAMADAKA
-            when morae
-              ODAKA
-            else
-              NAKADAKA
-            end
-        else
-          self.accent_pattern = nil
-        end
-      end
-    end
-
-    def count_morae
-      # get a throwaway copy of the reading, handling the nil case
-      string = characters.to_s.dup
-      # make sure to get only the first reading if there's more than one
-      string.sub!(/,.*/, "")
-      # count 1 for all hiragana, katakana and the katakana elongation symbol
-      full = string.each_char.map{ |c| c =~ /\A[ぁ-んァ-ンー]\z/ ? 1 : 0 }.sum
-      # count 1 for all small hiragana and katakana (but not the っ or ッ)
-      tiny = string.each_char.map{ |c| c =~ /\A[ぁァぃィぅゥぇェぉォゃャゅュょョ]\z/ ? 1 : 0 }.sum
-      # the number of morae is the difference between these two
-      full - tiny
     end
   end
 end
