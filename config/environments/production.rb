db0432594f05d795416ae4bd566f9e33e77ba56c
Mark Orr <mark.j.l.orr@googlemail.com>
2023-11-22 15:34:12 +0000

    Set ssl_options to be consistemt with other apps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = { hsts: false, redirect: false }
 
   # Log to STDOUT by default
   # config.logger = ActiveSupport::Logger.new(STDOUT)
