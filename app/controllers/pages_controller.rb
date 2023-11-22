7aa2176a61af0842a42221b2a5026de6cf75e3b0
Mark Orr <mark.j.l.orr@googlemail.com>
2023-11-22 13:12:26 +0000

    Enhanced env page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,30 +2,29 @@ class PagesController < ApplicationController
   # see https://github.com/CanCanCommunity/cancancan/wiki/Non-RESTful-Controllers
   authorize_resource class: false
 
-  def risle
-    @flats = Flat.where.not(bay: nil).order(:bay).all
+  def env
+    dirs = `ls /home/sanichi/.passenger/native_support`
+    vers = dirs.scan(/\d*\.\d*\.\d*/)
+    @passenger_version = vers.any? ? vers.last : "not found"
+    vers = ActiveRecord::Base.connection.execute('select version();').values[0][0] rescue "oops"
+    @postgres_version = vers.match(/(1[4-6]\.\d+)/)? $1 : "not found"
+    @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
   end
 
   def pam
     @answer = Flat.pam(params[:q])
   end
 
-  def weight
-    @kilos = Mass.kilos
-    @dates = Mass.dates
-    @unit = Mass::UNITS[params[:unit].try(:to_sym)] || Mass::DEFAULT_UNIT
+  def prefectures
+    @elements = Place.map_elements.to_a
   end
 
   def premier
     @data = helpers.premier_data(params[:season], params[:dun_due])
   end
 
-  def prefectures
-    @elements = Place.map_elements.to_a
-  end
-
-  def wordle
-    @wordle = Wordle.new(params)
+  def risle
+    @flats = Flat.where.not(bay: nil).order(:bay).all
   end
 
   def ruby
@@ -51,5 +50,15 @@ class PagesController < ApplicationController
     end
 
     render plain: answer
+  end
+
+  def weight
+    @kilos = Mass.kilos
+    @dates = Mass.dates
+    @unit = Mass::UNITS[params[:unit].try(:to_sym)] || Mass::DEFAULT_UNIT
+  end
+
+  def wordle
+    @wordle = Wordle.new(params)
   end
 end
