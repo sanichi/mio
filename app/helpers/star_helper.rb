62f1541d7750c680da1b2ed0920769427ce093a7
Mark Orr <mark.j.l.orr@googlemail.com>
2024-01-24 13:29:09 +0000

    Bug fix

--- app/helpers/star_helper.rb
+++ app/helpers/star_helper.rb
@@ -5,7 +5,7 @@ module StarHelper
   end
 
   def star_right_ascension(alpha)
-    if alpha.match(Star::ALPHA)
+    if alpha&.match(Star::ALPHA)
       "#{$1}<sup>h</sup> #{$2}<sup>m</sup> #{$3}<sup>s</sup>".html_safe
     else
       alpha
@@ -13,7 +13,7 @@ module StarHelper
   end
 
   def star_declination(delta)
-    if delta.match(Star::DELTA)
+    if delta&.match(Star::DELTA)
       "#{$1}#{$2}° #{$2}′ #{$3}″"
     else
       delta
