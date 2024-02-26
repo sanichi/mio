7c05e72a4b93786cf254254726e69eaa0d2c34b9
Mark Orr <mark.j.l.orr@googlemail.com>
2024-02-26 10:40:42 +0000

    Extra similar kanjis

---
+++ app/models/wk/tweak.rb
@@ -0,0 +1,20 @@
+module Wk
+  class Tweak
+    EXTRA_SIMILAR_KANJIS = {
+      1059 => 1868,
+    }
+
+    def self.update
+      EXTRA_SIMILAR_KANJIS.each do |id1, id2|
+        if id1 != id2
+          k1 = Wk::Kanji.find_by(wk_id: id1)
+          k2 = Wk::Kanji.find_by(wk_id: id2)
+          if k1 && k2
+            k1.similar_kanjis.push(k2) unless k1.similar_kanjis.include?(k2)
+            k2.similar_kanjis.push(k1) unless k2.similar_kanjis.include?(k1)
+          end
+        end
+      end
+    end
+  end
+end
