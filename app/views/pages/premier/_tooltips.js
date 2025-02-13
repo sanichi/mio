10e7300cdd41ed0dcca549f6e0945d676c5b4636
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-13 15:29:25 +0000

    Fix premier league table tooltips

--- app/views/pages/premier/_tooltips.js
+++ app/views/pages/premier/_tooltips.js
@@ -1,3 +1,8 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+document.addEventListener("turbo:visit", () => {
+  // const els = document.querySelectorAll('[data-toggle="tooltip"]');
+  // [...els].forEach((el) => { el.tooltip() }); FIX: el.tooltip is not a function
+  $('[data-toggle="tooltip"]').tooltip();
+});
