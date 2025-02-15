bbac696535de014604c227955e9f9e2a8f8dfae9
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-15 07:28:45 +0000

    Used controller for premier tooltips

--- app/views/pages/premier/_tooltips.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // TODO: this shouldn't be necessary once Turbo is on by default
-  $('[data-toggle="tooltip"]').tooltip();
-});
-document.addEventListener("turbo:visit", () => {
-  // const els = document.querySelectorAll('[data-toggle="tooltip"]');
-  // [...els].forEach((el) => { el.tooltip() }); FIX: el.tooltip is not a function
-  $('[data-toggle="tooltip"]').tooltip();
-});
