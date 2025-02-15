d60fb1b9f1bebe997870d68dbb87c2121cc2d0d4
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-15 08:15:00 +0000

    Bootstrap tooltips tweak

--- app/javascript/controllers/tooltips_controller.js
+++ app/javascript/controllers/tooltips_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     // Adapted from https://getbootstrap.com/docs/5.3/components/tooltips/#overview
-    const els = document.querySelectorAll('[data-toggle="tooltip"]');
+    const els = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     [...els].map(el => new bootstrap.Tooltip(el));
   }
 }
