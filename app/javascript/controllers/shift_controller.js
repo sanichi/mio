bd40d148df1f3108da6234b90bb854ff0bf5e84f
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-22 19:52:45 +0000

    Un-jquerified the shift controller

--- app/javascript/controllers/shift_controller.js
+++ app/javascript/controllers/shift_controller.js
@@ -8,18 +8,29 @@ export default class extends Controller {
   }
 
   go(e) {
-    const data = {
-      direction: e.params.d,
-      type: e.params.t,
-      delta: this.deltaTarget.value
+    const url = "/places/" + this.idValue + "/shift";
+    const options = {
+      method: "PATCH",
+      headers: {
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        direction: e.params.d,
+        type: e.params.t,
+        delta: this.deltaTarget.value,
+      }),
     };
-    $.ajax({
-      url: "/places/" + this.idValue + "/shift",
-      type: "patch",
-      data: data,
-      dataType: "html"
-    }).done(function(data) {
-      $('#map').html(data);
-    });
+    fetch(url, options)
+      .then(response => {
+        if (!response.ok) throw new Error("response error:", response.status);
+        return response.text();
+      })
+      .then(html => {
+        document.getElementById("map").innerHTML = html;
+      })
+      .catch(error => {
+        console.error("fetch error:", error);
+      });
   }
 }
