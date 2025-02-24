1db452868689a7e0485db4458b802530809c93ba
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-24 15:10:55 +0000

    Get rid of last jquery ajax calls in aoc and tree elm controllers

--- app/javascript/controllers/elm/aoc_controller.js
+++ app/javascript/controllers/elm/aoc_controller.js
@@ -6,38 +6,40 @@ export default class extends Controller {
     const app = Elm.Main.init({
       node: this.element,
       flags: {
-        year: '<%= year %>',
-        day: '<%= day %>'
+        year: "2021",
+        day: "1"
       }
     });
-    app.ports.getData.subscribe(function(yd) {
-      var year = yd[0];
-      var day  = yd[1];
-      var file = '/aoc/' + year + '/' + day + '.txt';
-      $.ajax({
-        url: file,
-        dataType: 'text'
-      }).done(function(data) {
-        app.ports.gotData.send(data);
-      });
+    app.ports.getData.subscribe((yd) => {
+      fetch(`/aoc/${yd[0]}/${yd[1]}.txt`)
+        .then(response => {
+          if (!response.ok) throw new Error("response error:", response.status);
+          return response.text();
+        })
+        .then(data => {
+          app.ports.gotData.send(data);
+        })
+        .catch(error => {
+          console.error("getData fetch error:", error);
+        });
     });
-    app.ports.getRuby.subscribe(function(ydp) {
-      $.ajax({
-        url: '/ruby',
-        data: {
-          year: ydp[0],
-          day:  ydp[1],
-          part: ydp[2]
-        },
-        dataType: 'text'
-      }).done(function(answer) {
-        app.ports.gotRuby.send([ydp[2], answer]);
-      });
+    app.ports.getRuby.subscribe((ydp) => {
+      fetch(`/ruby?year=${ydp[0]}&day=${ydp[1]}&part=${ydp[2]}`)
+        .then(response => {
+          if (!response.ok) throw new Error("response error:", response.status);
+          return response.text();
+        })
+        .then(data => {
+          app.ports.gotRuby.send([ydp[2], data]);
+        })
+        .catch(error => {
+          console.error("getRuby fetch error:", error);
+        });
     });
-    app.ports.doPause.subscribe(function(part) {
+    app.ports.doPause.subscribe((part) => {
       // this timeout is here so elm will pause long enough to display
       // the "thinking" gif before it starts calculating the answer
-      setTimeout(function() {
+      setTimeout(() => {
         app.ports.donePause.send(part);
       }, 100);
     });
--- app/javascript/controllers/elm/tree_controller.js
+++ app/javascript/controllers/elm/tree_controller.js
@@ -15,11 +15,11 @@ export default class extends Controller {
       node: this.elmTarget,
       flags: { focus: this.focusValue }
     });
-    this.app.ports.displayPerson.subscribe(function(id) {
-      window.location.href = "/people/" + id; // TODO: revisit this after turbo is opt-out
+    this.app.ports.displayPerson.subscribe((id) => {
+      Turbo.visit(`/people/${id}`);
     });
     const controller = this;
-    this.app.ports.getFocus.subscribe(function(id) {
+    this.app.ports.getFocus.subscribe((id) => {
       controller.getFocus(id);
     });
   }
@@ -27,15 +27,23 @@ export default class extends Controller {
   getFocus(id) {
     if (Number(id) === id && id % 1 === 0 && id > 0) {
       const controller = this;
-      $.ajax({
-        url: "/people/tree.json?id=" + id
-      }).done(function(focus) {
-        controller.gotFocus(focus);
-      });
+      fetch(`/people/tree.json?id=${id}`)
+        .then(response => {
+          if (!response.ok) throw new Error("response error:", response.status);
+          return response.json();
+        })
+        .then(data => {
+          controller.gotFocus(data);
+        })
+        .catch(error => {
+          console.error("focus fetch error:", error);
+        });
+    } else {
+      console.log("invalid person id:", id);
     }
   }
 
-  gotFocus(focus) {
-    this.app.ports.gotFocus.send(focus);
+  gotFocus(data) {
+    this.app.ports.gotFocus.send(data);
   }
 }
