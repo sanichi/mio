2f3d603bedaab2b9fc3e8fa3fbbe7027e69f0e49
Mark Orr <mark.j.l.orr@googlemail.com>
2025-04-28 15:30:26 +0100

    Gem update and spec --only-failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,9 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  # Allow --only-failures.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
