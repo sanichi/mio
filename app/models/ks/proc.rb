9ab6835e6f98e67839e024c91c36eccaead79181
Mark Orr <mark.j.l.orr@googlemail.com>
2025-04-30 21:48:19 +0100

    More information about command frequency

--- app/models/ks/proc.rb
+++ app/models/ks/proc.rb
@@ -14,77 +14,82 @@ module Ks
   end
 end
 
-# -bash
-# (sd-pam)
-# /home/sanichi/.rbenv/versions/3.4.2/bin/ruby /home/sanichi/.rbenv/versions/3.4.2/bin/bundle install
-# /usr/bin/python3 -s /usr/bin/certbot renew --noninteractive --no-random-sleep-on-renew
-# /usr/bin/python3 -s /usr/sbin/firewalld --nofork --nopid
-# /usr/bin/python3.9 /usr/bin/dnf makecache --timer
-# /usr/bin/sh /usr/libexec/pcp/bin/pmlogger_check --skip-primary
-# /usr/bin/sh /usr/libexec/pcp/bin/pmlogger_daily -K
-# /usr/bin/systemctl -s HUP kill rsyslog.service
-# /usr/lib/polkit-1/polkitd --no-debug
-# /usr/lib/systemd/systemd --switched-root --system --deserialize 31 rhgb
-# /usr/lib/systemd/systemd --user
-# /usr/lib/systemd/systemd-hostnamed
-# /usr/lib/systemd/systemd-journald
-# /usr/lib/systemd/systemd-logind
-# /usr/libexec/nm-dispatcher
-# /usr/libexec/pcp/bin/pmlogger -N -P -d "/var/log/pcp/pmlogger/LOCALHOSTNAME" -r -T24h10m -c config.d
-# /usr/libexec/sssd/sssd_kcm --uid 0 --gid 0 --logger=files
-# /usr/pgsql-17/bin/postgres -D /var/lib/pgsql/17/data/
-# /usr/sbin/CROND -n
-# /usr/sbin/httpd -DFOREGROUND
-# /usr/sbin/NetworkManager --no-daemon
-# /usr/sbin/rsyslogd -n
-# bash /home/sanichi/bin/kanshi.sh -bmp
-# gawk -f /home/sanichi/bin/toppu.awk
-# Passenger AppPreloader: /var/www/me.api/current
-# Passenger AppPreloader: /var/www/me.bid/current
-# Passenger AppPreloader: /var/www/me.chess/current
-# Passenger AppPreloader: /var/www/me.hou/current
-# Passenger AppPreloader: /var/www/me.mio/current
-# Passenger AppPreloader: /var/www/me.sj/current
-# Passenger AppPreloader: /var/www/me.sta/current
-# Passenger AppPreloader: /var/www/me.tmp/current
-# Passenger AppPreloader: /var/www/rek/current
-# Passenger AppPreloader: /var/www/smd/current
-# Passenger AppPreloader: /var/www/step/current
-# Passenger AppPreloader: /var/www/wd/current
-# Passenger core
-# Passenger RubyApp: /var/www/me.api/current (production)
-# Passenger RubyApp: /var/www/me.bid/current (production)
-# Passenger RubyApp: /var/www/me.chess/current (production)
-# Passenger RubyApp: /var/www/me.hou/current (production)
-# Passenger RubyApp: /var/www/me.mio/current (production)
-# Passenger RubyApp: /var/www/me.sj/current (production)
-# Passenger RubyApp: /var/www/me.sta/current (production)
-# Passenger RubyApp: /var/www/me.tmp/current (production)
-# Passenger RubyApp: /var/www/rek/current (production)
-# Passenger RubyApp: /var/www/smd/current (production)
-# Passenger RubyApp: /var/www/step/current (production)
-# Passenger RubyApp: /var/www/wd/current (production)
-# Passenger watchdog
-# postgres: autovacuum launcher
-# postgres: background writer
-# postgres: checkpointer
-# postgres: sanichi hou_production [local] idle
-# postgres: sanichi mio_production [local] idle
-# postgres: sanichi rek_production [local] idle
-# postgres: sanichi rek_production [local] idle in transaction
-# postgres: sanichi smd_production [local] idle
-# postgres: sanichi wd_production [local] idle
-# postgres: walwriter
-# ps -eo pid,rss,args --sort -rss
-# ruby bin/rails c
-# ruby bin/rails messages:get
-# sshd: [accepted]
-# sshd: [net]
-# sshd: ftp [priv]
-# sshd: postgres [priv]
-# sshd: root [net]
-# sshd: root [priv]
-# sshd: sanichi [priv]
-# sshd: sanichi@pts/0
-# sshd: unknown [net]
-# sshd: unknown [priv]
+#######################################################################################
+# Dump of the top (by %mem) commands found over a 2 week period with frequency.       #
+#######################################################################################
+
+# 69979 /usr/sbin/httpd -DFOREGROUND
+# 16364 Passenger core
+# 12305 /usr/lib/systemd/systemd --user
+# 10139 /usr/sbin/rsyslogd -n
+#  8275 /usr/lib/systemd/systemd --switched-root --system --deserialize 31 rhgb
+#  8222 Passenger RubyApp: /var/www/step/current (production)
+#  7368 postgres: checkpointer
+#  6527 Passenger RubyApp: /var/www/me.mio/current (production)
+#  6193 Passenger RubyApp: /var/www/wd/current (production)
+#  6127 Passenger RubyApp: /var/www/smd/current (production)
+#  5834 Passenger RubyApp: /var/www/me.hou/current (production)
+#  5426 Passenger RubyApp: /var/www/rek/current (production)
+#  4603 /usr/sbin/NetworkManager --no-daemon
+#  4485 postgres: sanichi mio_production [local] idle
+#  3886 /usr/libexec/pcp/bin/pmlogger -N -P -d "/var/log/pcp/pmlogger/LOCALHOSTNAME" -r -T24h10m -c config.d
+#  2732 /usr/sbin/CROND -n
+#  1645 sshd: [accepted]
+#   937 Passenger watchdog
+#   396 /usr/lib/systemd/systemd-journald
+#   339 /usr/lib/systemd/systemd-logind
+#   310 Passenger AppPreloader: /var/www/me.mio/current
+#   309 postgres: background writer
+#   300 sshd: root [priv]
+#   223 (sd-pam)
+#   204 postgres: sanichi wd_production [local] idle
+#   140 Passenger AppPreloader: /var/www/me.sj/current
+#   139 Passenger RubyApp: /var/www/me.sj/current (production)
+#   115 sshd: sanichi [priv]
+#   101 Passenger AppPreloader: /var/www/me.hou/current
+#    93 Passenger RubyApp: /var/www/me.chess/current (production)
+#    90 Passenger AppPreloader: /var/www/me.chess/current
+#    85 sshd: unknown [priv]
+#    65 Passenger AppPreloader: /var/www/me.tmp/current
+#    63 Passenger RubyApp: /var/www/me.tmp/current (production)
+#    60 Passenger AppPreloader: /var/www/wd/current
+#    52 /usr/libexec/nm-dispatcher
+#    50 sshd: [net]
+#    50 Passenger AppPreloader: /var/www/me.bid/current
+#    50 Passenger AppPreloader: /var/www/me.api/current
+#    49 postgres: sanichi rek_production [local] idle
+#    48 /usr/bin/python3 -s /usr/sbin/firewalld --nofork --nopid
+#    42 gawk -f /home/sanichi/bin/toppu.awk
+#    41 Passenger RubyApp: /var/www/me.bid/current (production)
+#    41 Passenger RubyApp: /var/www/me.api/current (production)
+#    31 Passenger AppPreloader: /var/www/me.sta/current
+#    30 Passenger RubyApp: /var/www/me.sta/current (production)
+#    30 Passenger AppPreloader: /var/www/smd/current
+#    26 /usr/bin/python3.9 /usr/bin/dnf makecache --timer
+#    25 /usr/libexec/sssd/sssd_kcm --uid 0 --gid 0 --logger=files
+#    15 Passenger AppPreloader: /var/www/step/current
+#    11 sshd: sanichi@pts/0
+#    11 postgres: sanichi smd_production [local] idle
+#    10 Passenger AppPreloader: /var/www/rek/current
+#    10 bash /home/sanichi/bin/kanshi.sh -bmp
+#     7 sshd: root [net]
+#     7 postgres: sanichi hou_production [local] idle
+#     6 /usr/bin/sh /usr/libexec/pcp/bin/pmlogger_check --skip-primary
+#     5 ps -eo pid,rss,args --sort -rss
+#     5 postgres: walwriter
+#     5 /usr/lib/systemd/systemd-hostnamed
+#     3 sshd: unknown [net]
+#     3 ruby bin/rails messages:get
+#     3 -bash
+#     2 sshd: postgres [priv]
+#     2 ruby bin/rails c
+#     2 /usr/pgsql-17/bin/postgres -D /var/lib/pgsql/17/data/
+#     2 /usr/bin/python3 -s /usr/bin/certbot renew --noninteractive --no-random-sleep-on-renew
+#     1 sshd: ftp [priv]
+#     1 postgres: sanichi rek_production [local] idle in transaction
+#     1 postgres: autovacuum launcher
+#     1 /usr/lib/polkit-1/polkitd --no-debug
+#     1 /usr/bin/systemctl -s HUP kill rsyslog.service
+#     1 /usr/bin/sh /usr/libexec/pcp/bin/pmlogger_daily -K
+#     1 /home/sanichi/.rbenv/versions/3.4.2/bin/ruby /home/sanichi/.rbenv/versions/3.4.2/bin/bundle install
+#  
