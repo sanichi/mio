a7929583fb57c79daad255a442d24e536addf09e
Mark Orr <mark.j.l.orr@googlemail.com>
2025-05-17 13:50:50 +0100

    Added goal difference to new stats page

--- app/models/team_stats.rb
+++ app/models/team_stats.rb
@@ -1,11 +1,12 @@
 class TeamStats
-  attr_reader :name, :names, :ids, :seasons, :totals, :home, :away
+  attr_reader :name, :names, :ids, :seasons, :pts, :gds, :home, :away
 
   def initialize(team)
     @name = team.name
     @seasons = []
     count = Hash.new(0)
-    @totals = Hash.new(0)
+    @pts = Hash.new(0)
+    @gds = Hash.new(0)
 
     @home = Hash.new { |h,k| h[k] = Hash.new }
     Match.where(home_team_id: team.id).each do |match|
@@ -14,7 +15,8 @@ class TeamStats
         pt = og == tg ? 1 : (og > tg ? 3 : 0)
 
         count[id] += 1
-        @totals[id] += pt
+        @pts[id] += pt
+        @gds[id] += og - tg
         @home[id][match.season] = [og, tg, pt]
         @seasons.push match.season unless @seasons.include?(match.season)
       end
@@ -27,7 +29,8 @@ class TeamStats
         pt = og == tg ? 1 : (og > tg ? 3 : 0)
 
         count[id] += 1
-        @totals[id] += pt
+        @pts[id] += pt
+        @gds[id] += og - tg
         @away[id][match.season] = [og, tg, pt]
         @seasons.push match.season unless @seasons.include?(match.season)
       end
@@ -42,6 +45,6 @@ class TeamStats
         @names[id] = short
       end
     end
-    @ids.sort! { |a,b| @totals[b] <=> @totals[a] }
+    @ids.sort! { |a,b| [@pts[b], @gds[b]] <=> [@pts[a], @gds[a]] }
   end
 end
