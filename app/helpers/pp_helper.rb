1bc5338b612bf43638135f5a0b309507fb5a0961
Mark Orr <mark.j.l.orr@googlemail.com>
2026-02-07 15:51:05 +0000

    Fix prices station menu bug

--- app/helpers/pp_helper.rb
+++ app/helpers/pp_helper.rb
@@ -14,7 +14,7 @@ module PpHelper
   end
 
   def pp_station_filter_menu(selected)
-    opts = Pp::Station.by_display_name.map { |s| [s.display_name, s.id] }
+    opts = Pp::Station.where(id: Pp::Price.select(:station_id)).by_display_name.map { |s| [s.display_name, s.id] }
     opts.unshift [t("all"), ""]
     options_for_select(opts, selected)
   end
